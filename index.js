e347fe13ca8f46e6a9b08046026d9d12eb3556fa
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-14 00:35:10 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+//  რიცხვის ჯამისალგორითმი
+// function summation(n){
+//     let sum = 0
+//     for(let i = 0; i <= n; i++){
+//         sum += i
+//     }
+//     return sum
+// }
+// console.log(summation(5))
+// function summation(n){
+//     return n*(n+1)/2
+// }
+// console.log(summation(5));
+// function printAllItems(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(arr[i]);
+//     }
+// }
+// console.log(printAllItems([1,2,3,4,5]))
+// math alghoritms 1.fibonacci sequence 2. factorialof a number 3. prime number 4. power of two 5. recursion 6. fibonacci sequence with recusion 7. factorial of a number with recursion
+//  fibonacci sequence
+// function fibonacci(n){
+//         const fib = [0,1]
+//         for(let i = 2; i <n;i++){
+//             fib[i] = fib[i-1] + fib[i-2]
+//         }
+//         return fib
+//     }
+//     console.log(fibonacci(2))//[0,1]
+//     console.log(fibonacci(3))//[0,1,1]
+//     console.log(fibonacci(7))//[0,1,1,2,3,5,8
+//JavaScript Algorithms - 8 - Factorial of a Number
+function factorial(n){
+    let sum = 1
+    for(let i = 2; i <= n; i++){
+        sum = sum * i 
+    }
+    return sum
+}
+console.log(factorial(5))
+// videoshi dawerili
+
