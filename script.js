ed717cb0923fb002409da7d0f68e2ba90860da1d
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-13 19:40:57 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -64,14 +64,72 @@
 // console.log(fibonacci(7))//[0,1,1,2,3,5,8
 // big-O -- 0(n)
 //JavaScript Algorithms - 8 - Factorial of a Number
-function factorial(n){
-    let result = 1;
-    for(let i = 2; i < n; i++){
-        result *= i;
-    }
-    return result;
-}
-console.log(factorial(0))//1
-console.log(factorial(1))//1
-console.log(factorial(5))//120
-// big-o = o(n)
+// function factorial(n){
+//     let result = 1;
+//     for(let i = 2; i < n; i++){
+//         result *= i;
+//     }
+//     return result;
+// }
+// console.log(factorial(0))//1
+// console.log(factorial(1))//1
+// console.log(factorial(5))//120
+// // big-o = o(n)
+//JavaScript Algorithms - 9 - Prime Number
+// function isPrime(n){
+//     if(n < 2){
+//         return false
+//     }
+//     for(let i = 2; i <= Math.sqrt(n); i++){
+//         if( n % i === 0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+// console.log(isPrime(1))//false  
+// console.log(isPrime(5))//true
+// console.log(isPrime(4))//false
+// big-O = O(n)
+//JavaScript Algorithms - 10 - Power of Two
+// function isPowerTwo(n){
+//     if(n < 1){
+//         return false
+//     }
+//     while(n > 1){
+//         if(n % 2!== 0){
+//             return false
+//         }
+//         n = n / 2
+//     }
+//     return true
+// }
+// console.log(isPowerTwo(1))//true
+// console.log(isPowerTwo(2))//true
+
+// console.log(isPowerTwo(5))//false
+
+// function isPowerTwoBitWise(n){
+//     if( n < 1){
+//         return false
+//     }
+//     return (n & (n - 1)) === 0
+// }
+// console.log(isPowerTwoBitWise(6))
+// big-) = O(logn)
+
+//JavaScript Algorithms - 11 - Recursion
+//JavaScript Algorithms - 12 - Recursive Fibonacci Sequence
+// function recursiveFibonacci(n){
+//     if(n < 2){
+//         return n
+//     }
+//     return recursiveFibonacci(n - 1) + recursiveFibonacci( n - 2)
+// }
+// console.log(recursiveFibonacci(0))//0
+// console.log(recursiveFibonacci(1))//1
+// console.log(recursiveFibonacci(6))//8
+// console.log(recursiveFibonacci(10))//8
+// O(n) - Iterative
+//JavaScript Algorithms - 13 - Recursive Factorial of a Number
+
