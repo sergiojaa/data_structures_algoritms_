9cd51d45117d18ba3cb6ecc7501a73922db91277
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-19 14:29:34 +0400

    Add files via upload

---
+++ algouni.js
@@ -0,0 +1,20 @@
+// alghoritm - წესების ერთობლიობა რომელიც გამოიყენება რომ output მივიღოთ 
+//მიღებული input-დან.
+// sorting algohritm- მასივის დალაგება
+// bubble sort alghoritm
+
+// let arr = [10,5,7,3,20,15,17,50]
+// let sortedArr = arr.sort((a,b)=> a - b)
+// console.log(sortedArr)
+// for(let i = 0; i < arr.length - 1;i++){ //path-ებისთვის,რამდენ path-ს გადის
+//     for(let j = 0;j < arr.length - 1 -i ; j++ ){  //რიცხვების დასალუპად,რო რიცხვები შევადაროთ ერთმანეთთან
+//         if(arr[j] > arr[j+1]){ //თუ  ერთი რიცხვი მეტია მეზობელ რიცხვზე, ეგ ორი რიცხვი უნდა ჩავანაცვლოთ
+//              let temp = arr[j]
+//              arr[j] = arr[j+1]
+//              arr[j+1] = temp
+//         }
+//     }                   
+// }
+// console.log(arr)
+
+//search alghoritms
