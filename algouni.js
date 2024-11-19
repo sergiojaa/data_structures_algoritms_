27901752557b256bbd28a010d73c954c49237be4
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-19 19:11:24 +0400

    Add files via upload

--- algouni.js
+++ algouni.js
@@ -18,3 +18,56 @@
 // console.log(arr)
 
 //search alghoritms
+//linear search
+// let arr = [10,5,7,3,20,15,17,50]
+// function linearSearch(arr,target){
+//     for(let i = 0;i < arr.length;i++){
+//         if(target === arr[i]){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+// console.log(linearSearch(arr,15))
+// binary search    // array dasortili unda ikos
+// let arr = [2,3,5,7,8,10,12,15,]
+// function binarySearch(arr,target){
+//     let low = 0 
+//     let high = arr.length - 1 
+//     while(low <= high){
+//         let mid = Math.floor((high + low) / 2)  
+//         if(arr[mid] === target){  // mid indexia
+//             return mid
+//         }else if(arr[mid] > target){
+//             high = mid - 1
+//         }else{ // tu meti araa da toli esigi nakklebia
+//             low = mid +1
+//         }
+//     }
+//     return -1
+// } 
+// console.log(binarySearch(arr, 12))
+
+// selection sort       //gareta cikli index ro gavigot,shignita ricxvebis shesadareblad
+let arr = [20,6,4,67,45,10,9,5];
+function selectionSort(arr){
+    for(let i = 0;i<arr.length-1;i++){
+        let maxIndex = 0
+
+        for(let j = 1;j< arr.length -i;j++){
+            if(arr[j] > arr[maxIndex] ){
+                maxIndex = j
+                
+            }
+        }
+        let temp = arr[maxIndex]
+        arr[maxIndex] = arr[arr.length - i -1]
+        arr[arr.length - i -1] = temp
+
+    }
+}
+selectionSort(arr)
+console.log(arr)
+
+
+
