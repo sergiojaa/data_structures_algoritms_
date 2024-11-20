e65af0e13a6bd03bd6c0fd4e6b014823cf63624c
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-20 19:27:58 +0400

    Add files via upload

--- taskAlgouni.js
+++ taskAlgouni.js
@@ -30,9 +30,9 @@
 // console.log(binarySearch(arr,18))
 
 //bubble sort
-// let arr = [10,5,7,3,20,15,17,50]
+// let arr = [10,1,2,3,5,4,9,8]
 // for(let i = 0; i < arr.length - 1;i++){  // indexs itvlis
-//     for(let j = 0;j<arr.length-1;j++){ // რიცხვების დალუპვა რო მერე შევადაროთ
+//     for(let j = 0;j < arr.length - 1 - i ;j++){ // რიცხვების დალუპვა რო მერე შევადაროთ
         
 //         if(arr[j] > arr[j+1]){
 //             let temp = arr[j]
@@ -42,17 +42,44 @@
 //     }
 // }
 // console.log(arr)
+
+// let arr = [10,1,2,3,5,4,9,8]
+// for(let i = 0; i < arr.length - 1;i++){
+//     for(let j = 0;j < arr.length - 1 - i;j++){
+//         if(arr[j] > arr[j+1]){
+//             let temp = arr[j]
+//             arr[j] = arr[j+1]
+//             arr[j+1] = temp
+//         }
+//     }
+// }
+// console.log(arr)
+
 // linear araa maincdamainc rekomendirebuli,radgan tavidan bolomde
 //uvlis masivs da tu target bolos aris did dros moandomebs
 //roca did data structurastan mogviwevs mushaoba cudi prakikaa mitumeres
 //binary is better
-const arr = [2,4,3,5,7,8,]
-function linearSearch(arr,target){
-    for(let i = 0;i<arr.length;i++){
-        if(target === arr[i]){
-            return i
+let arr = [2,4,6,8,10,12,14,16,18]
+function binarySearch(arr,target){
+    let low = 0
+    let high = arr.length - 1
+    while(low <= high){
+        let mid = Math.floor((low + high) / 2)
+        if(arr[mid] === target){
+            return mid
+        }else if(arr[mid] > target){
+            high = mid - 1
+        }else{
+            low = mid + 1
         }
     }
-    return -1
 }
-console.log(linearSearch(arr,7))
+// if(arr[mid] === target){
+    //             return mid
+    //         }else if(arr[mid] > target){
+    //             high = mid - 1
+    //         }else{
+    //             low = mid + 1
+    //         }
+    //     }
+console.log(binarySearch(arr,12))
